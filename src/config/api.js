11f0140af3e5fdce592a08f7eacd66f4042fa90d
Hafidh Rizky <hafidhrzk@gmail.com>
2022-08-06 13:25:06 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 
+  baseURL: process.env.REACT_APP_SERVER_URL ||
     'https://backend-friendlist-app.herokuapp.com/api/v1' || 
     'http://localhost:5000/api/v1/',
 });
