304fdf85b47e6859952860a3f08f66d0b0341097
Hafidh Rizky <hafidhrzk@gmail.com>
2022-08-06 14:10:53 +0700

    update 4

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -5,11 +5,6 @@ export default function Navibar() {
     return(
         <Navbar bg="secondary" variant="dark">
             <Container>
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Friend List</Nav.Link>
-                    <Nav.Link href="/inputlist">Input List</Nav.Link>
-                    <Nav.Link href="/chart">Chart</Nav.Link>
-                </Nav>
                 <Nav className="ms-auto">
                     <Nav.Link as={Link} to="/" className="text-navbar">Friend List</Nav.Link>
                     <Nav.Link as={Link} to="/inputlist" className="text-navbar">Input List</Nav.Link>
